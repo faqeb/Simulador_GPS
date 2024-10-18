c1e8474e30bb23f4d72306c81589ea2684ccd03b
Facundo Baez <143283286+faqeb@users.noreply.github.com>
2024-10-18 15:54:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -264,7 +264,29 @@ def update_location():
         return jsonify({'error': 'Fallo al enviar la ubicación a Traccar.'}), 500
 
     return jsonify({'message': f'Ubicación del dispositivo {id} actualizada a lat: {lat}, lon: {lon}'}), 200
+    
+def calculate_distance(lat1, lon1, lat2, lon2):
+    # Radio de la Tierra en kilómetros
+    R = 6371.0
 
+    # Convertir grados a radianes
+    lat1_rad = math.radians(lat1)
+    lon1_rad = math.radians(lon1)
+    lat2_rad = math.radians(lat2)
+    lon2_rad = math.radians(lon2)
+
+    # Diferencias entre las coordenadas
+    dlat = lat2_rad - lat1_rad
+    dlon = lon2_rad - lon1_rad
+
+    # Fórmula de haversine
+    a = math.sin(dlat / 2)**2 + math.cos(lat1_rad) * math.cos(lat2_rad) * math.sin(dlon / 2)**2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+
+    # Distancia en kilómetros
+    distance = R * c
+
+    return distance
 
 if __name__ == '__main__':
     app.run(debug=True)
